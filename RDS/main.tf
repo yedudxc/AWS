72facb46271a060c4e7d008fb0de15a63e3674e0
yedudxc <69684639+yedudxc@users.noreply.github.com>
2020-10-17 14:47:57 +0530

    Update main.tf
    updated code

--- RDS/main.tf
+++ RDS/main.tf
@@ -111,22 +111,5 @@ resource "aws_db_instance" "db" {
 }
 
 
-#-------OUTPUTS ------------
 
-output "Database Name" {
-  value = "${var.dbname}"
-}
-
-output "Database Hostname" {
-  value = "${aws_db_instance.db.endpoint}"
-}
-
-output "Database Username" {
-  value = "${var.dbuser}"
-}
-
-output "Database Password" {
-  value = "${random_string.postgres_password.result}" 
-  sensitive   = true
-}
 
